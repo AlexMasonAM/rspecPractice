fd4329c3d81de3019bfc6db0c3cd4a1b8dc9ebb2
AlexMasonAM <alexmmason@gmail.com>
2015-01-15 17:11:53 -0800

    updated tests with Faker gem info

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -9,36 +9,46 @@ RSpec.describe "Basic Tests" do
     expect(10.to_s).to eq("10")
   end
 
+  it "to_s should equal a string type" do
+    expect(10.to_s).to be_a(String)
+  end
+
   it "1 should not equal '1'" do
-    expect(1).not_to eq("1")
+    expect(1).not_to eq('1')
   end
 
   it "Expect 1 to be greater than 0" do
     expect(1).to be > 0
   end
 
-  it "Expects [1, 2, 3] to include 2" do
+  it "'this string' should start with this" do
+    expect("this string").to start_with("this")
+  end
+
+  it "'this string' should ends with ing" do
+    expect("this string").to end_with("ing")
+  end
+
+  it "Expects [1,2,3] to include 2" do
     expect([1,2,3]).to include(2)
   end
-
-  it "'this string' should start with this" do
-    expect("this string").to start_with('this')
-  end
-
 
 end
 
 RSpec.describe Contact do
+  let(:firstname) { Faker::Name.first_name }
+  let(:lastname) { Faker::Name.last_name }
+  let(:email) { Faker::Internet.email }
 
-before(:each) do
+  before(:each) do
     @contact = Contact.new(
-      firstname: "Alex",
-      lastname: "Mason",
-      email: "alex@mason.am"
+      firstname: firstname,
+      lastname: lastname,
+      email: email
     )
   end
 
-  after(:each) do 
+  after(:each) do
     Contact.delete_all
   end
 
@@ -59,22 +69,16 @@ before(:each) do
 
   it "should validate uniqueness of email" do
     @contact2 = Contact.new(
-      firstname: "Me",
-      lastname: "Oryou",
-      email: "alex@mason.am"
-      )
-    expect(@contact.save).to eq(true) 
-    expect(@contact2.save).to eq(false) 
+      firstname: "Fred",
+      lastname: "Williams",
+      email: @contact.email
+    )
+    expect(@contact.save).to eq(true)
+    expect(@contact2.save).to eq(false)
   end
 
   it "returns a contact's full name as a string" do
-    @contact2 = Contact.new(
-      firstname: "Me",
-      lastname: "Orewe",
-      email: "me@orewe.mo"
-    )
-    expect(@contact.fullname).to eq("Alex Mason")
-    expect(@contact2.fullname).to eq("Me Orewe")
+    expect(@contact.fullname).to eq("#{@contact.firstname} #{@contact.lastname}")
   end
 
 end
